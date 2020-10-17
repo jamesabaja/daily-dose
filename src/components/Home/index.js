2e4761db1c1dba0d066e05146f286d347ae97685
kimberrypi <ksmorano@gmail.com>
2020-10-17 09:12:06 +0800

    update css: removed bulma packageupdate

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
-import classNames from "classnames"
 
-import styles from "./utils/home.module.scss"
+
+import "./utils/home.scss"
 import drugs from "../../../static/drugs.json"
 import { getLocalStorageConfig, setLocalStorageConfig } from "./services/home"
 
@@ -18,16 +18,16 @@ const Home = () => {
   }, [])
 
   return (
-    <div className={classNames("has-text-centered", styles["home"])}>
-      <h1 className="title mb-0">{drug?.name}</h1>
-      <p className="is-size-4 is-italic mb-5">{drug?.class}</p>
-      <p className="is-size-4 mt-4">{drug?.indication}</p>
-      <div className={classNames("tags mt-5 mx-5", styles["home__tags"])}>
+    <main className="home">
+      <h1>{drug?.name}</h1>
+      <h3>{drug?.class}</h3>
+      <h5>{drug?.indication}</h5>
+      <div className="tags">
         {drug?.conditions?.slice(0, 5)?.map(condition => (
-          <span class="tag">{condition}</span>
+          <span className="tags__tag">{condition}</span>
         ))}
       </div>
-    </div>
+    </main>
   )
 }
 
